daf064e5e2bc69bb9225ad0acc4c7374f15504b5
Mayur Gohel <mayurgohel915@gmail.com>
2018-06-15 12:04:31 +0530

    Initial Commit

---
+++ DynaTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynaTask
+//
+//  Created by MayurGohel on 15/06/18.
+//  Copyright © 2018 Dynamic Microlink Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
